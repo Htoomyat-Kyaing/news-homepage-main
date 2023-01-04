91c10e2408a8827b132748b9b886e9be3aea5958
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-01-04 08:26:01 +0630

    dark filter for mobile nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 const navMobile = document.querySelector(".navigation_mobile");
 const navList = document.querySelector(".navigation_list");
+const darkFilter = document.querySelector(".darkFilter");
 
 navMobile.addEventListener("click", () => {
   navList.classList.toggle("open");
   navMobile.classList.toggle("toggle");
+  darkFilter.classList.toggle("toggle");
 });
