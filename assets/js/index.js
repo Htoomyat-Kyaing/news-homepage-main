21139d1be07cb268b73cdc3f3e98bb5b327826c7
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-01-05 18:36:55 +0630

    disable scroll while in mobile menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,31 @@
 const navMobile = document.querySelector(".navigation_mobile");
 const navList = document.querySelector(".navigation_list");
 const darkFilter = document.querySelector(".darkFilter");
+let canScroll = true;
 
 navMobile.addEventListener("click", () => {
   navList.classList.toggle("open");
   navMobile.classList.toggle("toggle");
   darkFilter.classList.toggle("toggle");
+  toggleScroll();
 });
+
+function toggleScroll() {
+  canScroll ? disableScroll() : enableScroll();
+}
+
+function disableScroll() {
+  // Get the current page scroll position
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+    // if any scroll is attempted, set this to the previous value
+    (window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    });
+  canScroll = false;
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+  canScroll = true;
+}
